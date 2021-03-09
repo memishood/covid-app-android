32fdeb58ade62010b605b2d9d24dd2b28a1a9966
memishood <emre.memis.49@gmail.com>
2021-03-09 21:24:15 +0300

    update main activity

--- app/src/main/java/com/covidapp/MainActivity.kt
+++ app/src/main/java/com/covidapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.covidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.covidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
