4650f6295bb3cd1eefddd9278d289d13c5146dc6
memishood <emre.memis.49@gmail.com>
2021-03-09 21:24:26 +0300

    create home fragment

---
+++ app/src/main/java/com/covidapp/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.covidapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.covidapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+
+}
