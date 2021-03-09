f5a2d037c3d77efb55cf95d66d28c51bb6be10b3
memishood <emre.memis.49@gmail.com>
2021-03-09 21:32:49 +0300

    update main activity

--- app/src/main/java/com/covidapp/MainActivity.kt
+++ app/src/main/java/com/covidapp/MainActivity.kt
@@ -2,13 +2,47 @@ package com.covidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.covidapp.databinding.ActivityMainBinding
+import com.covidapp.ext.Extensions.runOnHandler
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private var appExitStatus = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navController = findNavController(R.id.fragment)
+    }
+
+    override fun onBackPressed() {
+        when (navController.navigateUp()) {
+            true -> super.onBackPressed()
+            false -> prepareExit()
+        }
+    }
+
+    private fun prepareExit() {
+        when (appExitStatus) {
+            true -> finishAffinity()
+            else -> appExitStatus = true
+        }
+        Snackbar.make(
+            binding.root,
+            getString(R.string.app_exit_status_message),
+            Snackbar.LENGTH_SHORT
+        ).show()
+        runOnHandler(APP_EXIT_MILLS_TIME) {
+            appExitStatus = false
+        }
+    }
+
+    companion object {
+        private const val APP_EXIT_MILLS_TIME = 1500L
     }
 }
