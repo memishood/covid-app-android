7adf212f19be2331655ce160c3ba51ce9594c5b0
memishood <emre.memis.49@gmail.com>
2021-03-20 14:27:29 +0300

    fix timeout error

--- app/src/main/java/com/covidapp/di/NetworkModule.kt
+++ app/src/main/java/com/covidapp/di/NetworkModule.kt
@@ -5,8 +5,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 /**
@@ -17,17 +19,36 @@ import javax.inject.Singleton
 @Module
 object NetworkModule {
     private const val BASE_URL = "https://covid-api-backend.herokuapp.com/"
+    private const val CALL_TIMEOUT = 45 * 1000L
+    private const val CONNECT_TIMEOUT = 25 * 1000L
+    private const val READ_TIMEOUT = 45 * 1000L
 
     /**
-     * create a retrofit instance
+     * create a http client for retrofit
      */
     @Provides
     @Singleton
-    fun retrofitProvides() : Retrofit {
+    fun httpClientProvides() : OkHttpClient {
+        return OkHttpClient.Builder()
+                .callTimeout(CALL_TIMEOUT, TimeUnit.MILLISECONDS)
+                .connectTimeout(CONNECT_TIMEOUT, TimeUnit.MILLISECONDS)
+                .readTimeout(READ_TIMEOUT, TimeUnit.MILLISECONDS)
+                .build()
+    }
+
+    /**
+     * create a retrofit instance
+     * @param httpClient from dependency injection
+     * @see httpClientProvides
+     */
+    @Provides
+    @Singleton
+    fun retrofitProvides(httpClient: OkHttpClient) : Retrofit {
         return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient)
+                .build()
     }
 
     /**
